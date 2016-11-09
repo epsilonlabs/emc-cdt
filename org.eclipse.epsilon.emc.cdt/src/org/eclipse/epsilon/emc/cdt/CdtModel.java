85a417192a2949d0b50e6b0808aae2a2146d32a3
Simos Gerasimou <simos@cs.york.ac.uk>
2016-11-09 22:45:43 +0000

    -added functionality to ReflectiveASTVisitor - added functionality to CdtModel (implemented some functions and the others throw an UnsupportedOperationException)

--- org.eclipse.epsilon.emc.cdt/src/org/eclipse/epsilon/emc/cdt/CdtModel.java
+++ org.eclipse.epsilon.emc.cdt/src/org/eclipse/epsilon/emc/cdt/CdtModel.java
@@ -1,138 +1,239 @@
+/*******************************************************************************
+ * Copyright (c) 2016 University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Simos Gerasimou - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.epsilon.emc.cdt;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
-import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.model.ICElement;
+import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.exceptions.models.EolEnumerationValueNotFoundException;
 import org.eclipse.epsilon.eol.exceptions.models.EolModelElementTypeNotFoundException;
 import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
 import org.eclipse.epsilon.eol.exceptions.models.EolNotInstantiableModelElementTypeException;
 import org.eclipse.epsilon.eol.models.CachedModel;
+import org.eclipse.epsilon.eol.models.IRelativePathResolver;
 
 public class CdtModel extends CachedModel<Object>{
 
+	/** list of supported types*/
+	protected List<String> supportedTypes = Arrays.asList("ICElement", "ICProject");
+
+	
+	/** denotes property projects*/
+	public static final String PROPERTY_PROJECT = "cproject";
+	
+	/** denotes whether bindings should be resolved*/
+	public static final String PROPERTY_RESOLVE_BINDINGS = "resolveBindings";
+
+	/** C project*/
+	protected ICProject project = null;
+	
+	/** Resolve bindings flag*/
+	private boolean resolveBindings = false;
+
+	/** Visitor element*/
+	private ReflectiveASTVisitor visitor = null;
+	
+	
 	@Override
 	public Object getEnumerationValue(String enumeration, String label) throws EolEnumerationValueNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println(getClass().getSimpleName() +".getEnumerationValue(..)");
+		throw new UnsupportedOperationException("getEnumerationValue(..) not Implemented");
 	}
 
 	@Override
-	public String getTypeNameOf(Object instance) {
-		// TODO Auto-generated method stub
-		return null;
+ 	public String getTypeNameOf(Object instance) {
+		System.out.println(getClass().getSimpleName() +".getTypeNameOf(..)");
+		throw new UnsupportedOperationException("getTypeNameOf(..) not Implemented");
 	}
 
 	@Override
-	public Object getElementById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+ 	public Object getElementById(String id) {
+		System.out.println(getClass().getSimpleName() +".getElementById(..)");
+		throw new UnsupportedOperationException("getElementById(..) not Implemented");
 	}
 
 	@Override
-	public String getElementId(Object instance) {
-		// TODO Auto-generated method stub
-		return null;
+ 	public String getElementId(Object instance) {
+		System.out.println(getClass().getSimpleName() +".getElementId(..)");
+		throw new UnsupportedOperationException("getElementId(..) not Implemented");
 	}
 
 	@Override
-	public void setElementId(Object instance, String newId) {
-		// TODO Auto-generated method stub
-		
+ 	public void setElementId(Object instance, String newId) {
+		System.out.println(getClass().getSimpleName() +".setElementId(..)");
+		throw new UnsupportedOperationException("setElementId(..) not Implemented");
+	}
+
+
+	@Override
+ 	public boolean isInstantiable(String type) {
+		System.out.println(getClass().getSimpleName() +".isInstantiable(..)");
+		throw new UnsupportedOperationException("isInstantiable(..) not Implemented");
 	}
 
 	@Override
-	public boolean owns(Object instance) {
-		// TODO Auto-generated method stub
-		return false;
+ 	public boolean hasType(String type) {
+		try{
+//			System.out.println(getClass().getSimpleName() +".hasType(..)");
+			return supportedTypes.contains(type) || (Class.forName("org.eclipse.cdt.core.dom.ast.IAST" + type) != null);
+		} 
+		catch (ClassNotFoundException e) {
+			return false;
+		}		
 	}
 
+	
 	@Override
-	public boolean isInstantiable(String type) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean hasType(String type) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean store(String location) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean store() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	protected Collection<? extends Object> allContentsFromModel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected Collection<? extends Object> getAllOfTypeFromModel(String type)
-			throws EolModelElementTypeNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected Collection<? extends Object> getAllOfKindFromModel(String kind)
-			throws EolModelElementTypeNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected Object createInstanceInModel(String type)
-			throws EolModelElementTypeNotFoundException, EolNotInstantiableModelElementTypeException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected void loadModel() throws EolModelLoadingException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void disposeModel() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected boolean deleteElementInModel(Object instance) throws EolRuntimeException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	protected Object getCacheKeyForType(String type) throws EolModelElementTypeNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected Collection<String> getAllTypeNamesOf(Object instance) {
-		// TODO Auto-generated method stub
-		return null;
+ 	protected Collection<? extends Object> allContentsFromModel() {
+		System.out.println(getClass().getSimpleName() +".allContentsFromModel(..)");
+		throw new UnsupportedOperationException("allContentsFromModel(..) not Implemented");
 	}
 	
 	
-	private void test(){
-		ASTVisitor visitor = new ASTVisitor() {
-		};
+	@Override
+ 	public Collection<Object> getAllOfKind(String kind) throws EolModelElementTypeNotFoundException {
+		System.out.println(getClass().getSimpleName() +".getAllOfKind(..)");
+		return super.getAllOfKind(kind);
 	}
 
+	
+ 	public Collection<Object> getAllOfType(String type) throws EolModelElementTypeNotFoundException {
+		System.out.println(getClass().getSimpleName() +".getAllOfType(..)");
+		return super.getAllOfType(type);
+	}
+	
+	
+	@Override
+ 	protected Collection<Object> getAllOfKindFromModel(String kind) throws EolModelElementTypeNotFoundException {
+		System.out.println(getClass().getSimpleName() +".getAllOfKindFromModel(..)");
+		return getAllOfTypeFromModel(kind);
+	}
+	
+	
+	@Override
+ 	protected Collection<Object> getAllOfTypeFromModel(String type) throws EolModelElementTypeNotFoundException {
+		System.out.println(getClass().getSimpleName() +".getAllOfTypeFromModel(..)");
+		return visitor.getAllofType(type);
+	}
+
+	
+	@Override
+ 	protected Object createInstanceInModel(String type) throws EolModelElementTypeNotFoundException, EolNotInstantiableModelElementTypeException {
+		System.out.println(getClass().getSimpleName() +".createInstanceInModel(..)");
+		throw new UnsupportedOperationException("createInstanceInModel(..) not Implemented");
+	}
+
+	
+	@Override
+ 	protected void disposeModel() {
+//		System.out.println(getClass().getSimpleName() +".disposeModel(..)");
+		project = null;
+		resolveBindings = false;
+		visitor = null;
+	}
+
+	
+	@Override
+ 	protected boolean deleteElementInModel(Object instance) throws EolRuntimeException {
+		System.out.println(getClass().getSimpleName() +".deleteElementInModel(..)");
+		throw new UnsupportedOperationException("deleteElementInModel(..) not Implemented");
+	}
+ 
+	
+	@Override
+ 	protected Object getCacheKeyForType(String type) throws EolModelElementTypeNotFoundException {
+		System.out.println(getClass().getSimpleName() +".getCacheKeyForType(..)");
+		return type;
+	}
+
+	
+	@Override
+ 	protected Collection<String> getAllTypeNamesOf(Object instance) {
+		System.out.println(getClass().getSimpleName() +".getAllTypeNamesOf(..)");
+		throw new UnsupportedOperationException("getAllTypeNamesOf(..) not Implemented");
+	}
+	
+	
+	/**
+	 *  Load properties
+	 */
+	@Override
+ 	public void load(StringProperties properties, IRelativePathResolver resolver) throws EolModelLoadingException {
+		super.load(properties, resolver);
+		
+		//get project name
+		String projectName = properties.getProperty(CdtModel.PROPERTY_PROJECT);
+		try {
+			project = CdtUtil.getICProject(projectName);
+		} catch (CoreException e) {
+			throw new EolModelLoadingException(e, this);
+		}
+		
+		//get resolve bindings flag
+		resolveBindings = Boolean.parseBoolean(properties.getProperty(PROPERTY_RESOLVE_BINDINGS));
+		
+		//init visitor
+		visitor = new ReflectiveASTVisitor(project, resolveBindings);
+		
+		//finally load model
+		load();
+	}
+	
+	
+	/**
+	 * Load model
+	 */
+	@Override
+ 	protected void loadModel() throws EolModelLoadingException {
+		
+	}
+
+	
+	/**
+	 * Check whether it is the given object is owned
+	 */
+	@Override
+ 	public boolean owns (Object object){
+		if ( (object instanceof ICElement) || (object instanceof IASTNode) ){
+			return true;
+		}
+		return false;
+	}
+
+	
+	/* (non-Javadoc) not implemented
+	 * @see org.eclipse.epsilon.eol.models.IModel#store(java.lang.String)
+	 */
+	@Override
+ 	public boolean store(String location) {
+		System.out.println(getClass().getSimpleName() +".store(..)");
+		throw new UnsupportedOperationException("store(..) not Implemented");
+	}
+
+	
+	/* (non-Javadoc) not implemented
+	 * @see org.eclipse.epsilon.eol.models.IModel#store()
+	 */
+	@Override
+ 	public boolean store() {
+		System.out.println(getClass().getSimpleName() +".store(..)");
+		throw new UnsupportedOperationException("store(..) not Implemented");
+	}
+
+	
 }
--- org.eclipse.epsilon.emc.cdt/src/org/eclipse/epsilon/emc/cdt/ReflectiveASTVisitor.java
+++ org.eclipse.epsilon.emc.cdt/src/org/eclipse/epsilon/emc/cdt/ReflectiveASTVisitor.java
@@ -1,53 +1,335 @@
+/*******************************************************************************
+ * Copyright (c) 2016 University of York.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Simos Gerasimou - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.epsilon.emc.cdt;
 
-import org.eclipse.cdt.core.dom. ast.ASTVisitor;
-import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
-import org.eclipse.cdt.core.model.CModelException;
-import org.eclipse.cdt.core.model.ICProject;
-import org.eclipse.cdt.core.model.IPathEntry;
-import org.eclipse.core.runtime.IPath;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.rmi.UnexpectedException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Logger;
 
-public class ReflectiveASTVisitor extends ASTVisitor {
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.dom.ast.ASTGenericVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.index.IIndex;
+import org.eclipse.cdt.core.model.CoreModelUtil;
+import org.eclipse.cdt.core.model.ICElement;
+import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.cdt.core.model.ISourceRoot;
+import org.eclipse.cdt.core.model.ITranslationUnit;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+public class ReflectiveASTVisitor extends ASTGenericVisitor {
 
 	/** project */
-	protected ICProject project = null; 
+	protected ICProject cproject = null;
 
-	/** parser*/
+	/** parser */
 	protected IASTTranslationUnit parser = null;
-	
+
+	/** Resolve bindings flag */
+	private boolean resolveBindings = false;
+
+	/** Source file that includes a main function of the c program*/
+	ITranslationUnit mainTU = null;
+
+	/** Main AST */
+	IASTTranslationUnit mainAST = null;
+
 	
 	/**
 	 * Class constructor: initialise a ReflectiveASTVisitor
+	 * 
 	 * @param aProject
 	 */
-	public ReflectiveASTVisitor (ICProject aProject){
-		this.project = aProject;
+	public ReflectiveASTVisitor(ICProject project, boolean resolveBindings) {
+		super(true);
+		try {
+			this.cproject = project;
+			this.resolveBindings = resolveBindings;
+			findMainFunction();
+		} 
+		catch (NoSuchMethodException | UnexpectedException | CoreException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	/**
+	 * Return the AST for a project
+	 * 
+	 * @return IASTTranslationUnit
+	 * @throws CoreException 
+	 * @throws UnexpectedException 
+	 */
+	protected IASTTranslationUnit getASTForFile(IFile file) throws CoreException, UnexpectedException {		
+		// 1) get AST for a workspace file
+		// if (parser == null){
+		// //Create translation unit for file
+		// ITranslationUnit tu = CoreModelUtil.findTranslationUnit(file);
+		// if (tu == null){
+		// Shell shell
+		// =PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		// MessageDialog.openInformation( shell, "Unexpected error in
+		// translation unit", "Unexpected error in translation unit (NULL)");
+		// throw new UnexpectedException("Unexpected error in translation unit
+		// (NULL)");
+		// }
+		// //get AST
+		// IASTTranslationUnit iast = tu.getAST();
+		// }
+
+		// 2) get an AST for a code reader (i.e., adding additional properties)
+		// if (parser == null){
+		// FileContent fileContent =
+		// FileContent.createForExternalFileLocation(file.getRawLocation().toString());
+		// IScannerInfo info = new ScannerInfo(new HashMap<String, String>());
+		// IncludeFileContentProvider emptyIncludes =
+		// IncludeFileContentProvider.getEmptyFilesProvider();
+		// IIndex index = EmptyCIndex.INSTANCE;
+		// int options = ITranslationUnit.AST_SKIP_ALL_HEADERS |
+		// GPPLanguage.OPTION_NO_IMAGE_LOCATIONS |
+		// GPPLanguage.OPTION_IS_SOURCE_UNIT;
+		// IParserLogService log = new DefaultLogService();
+		//
+		// parser = GPPLanguage.getDefault().getASTTranslationUnit(fileContent,
+		// info, emptyIncludes, index, options , log);
+		// }
+
+		// 3) get an index-based AST for a code reader
+		if (parser == null) {
+			// Create translation unit for file
+			ITranslationUnit tu = CoreModelUtil.findTranslationUnit(file);
+			if (tu == null) {
+				Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+				MessageDialog.openInformation(shell, "Unexpected error in translation unit",
+						"Unexpected error in translation unit (NULL)");
+				throw new UnexpectedException("Unexpected error in translation unit (NULL)");
+			}
+			//create index
+			IIndex index = CCorePlugin.getIndexManager().getIndex(tu.getCProject());
+			// get AST
+			IASTTranslationUnit iast = tu.getAST(index, ITranslationUnit.AST_SKIP_INDEXED_HEADERS);
+			iast.accept(this);
+		}
+
+		// try {
+		// for (IPathEntry child : project.getResolvedPathEntries()){
+		// System.out.println(child.getPath().toString());
+		// }
+		// } catch (CModelException e) {
+		// e.printStackTrace();
+		// }
+
+		return null;
+	}
+	
+
+	/**
+	 * Find the source file with main() function
+	 * @param cProject
+	 * @return
+	 * @throws NoSuchMethodException
+	 * @throws UnexpectedException
+	 * @throws CoreException
+	 * @throws ClassNotFoundException 
+	 */
+	public void findMainFunction() throws NoSuchMethodException, UnexpectedException, CoreException, ClassNotFoundException{
+		//get source folders
+		for (ISourceRoot sourceRoot : cproject.getSourceRoots()){
+//			System.out.println(sourceRoot.getLocationURI().getPath());
+			//get all elements
+			for (ICElement element : sourceRoot.getChildren()){
+//				System.out.println(element.getElementName() +"\t"+ element.getClass());
+				//find a .c or cpp file
+				if (element instanceof ITranslationUnit && ((ITranslationUnit) element).isSourceUnit()){
+					ITranslationUnit tu		= (ITranslationUnit) element;
+					IASTTranslationUnit ast = getASTFromtTranslationUnit(tu);
+					IASTNode node = findNodeInTree(ast, 1, Class.forName("org.eclipse.cdt.core.dom.ast.IASTFunctionDeclarator"), "main");
+					if (node != null && mainTU == null){
+						mainTU = tu;
+						mainAST = ast;
+					}
+					else if (node != null && mainTU != null){
+						throw new NoSuchMethodException(String.format("Multiple main() methods in project %s", cproject.getProject().getName()));
+					} 
+				}
+				
+			}
+		}
+		
+		if (mainTU ==null)
+			throw new NoSuchMethodException(String.format("main() method not found in project %s", cproject.getProject().getName()));
+		return;
 	}
 	
 	
 	/**
-	 * Return the AST for a project
-	 * @return IASTTranslationUnit
+	 * Get AST from translation unit
+	 * @param tu
+	 * @param cproject
+	 * @param resolveBindings
+	 * @return
+	 * @throws UnexpectedException
+	 * @throws CoreException
 	 */
-	protected IASTTranslationUnit getASTParser(){
+	private IASTTranslationUnit getASTFromtTranslationUnit(ITranslationUnit tu) throws UnexpectedException, CoreException{
+		//check if tu is null
+		if (tu == null){
+			Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+			MessageDialog.openInformation( shell, "Unexpected error in translation unit", "Unexpected error in translation unit (NULL)");
+			throw new UnexpectedException("Unexpected error in translation unit (NULL)");
+		}
+		//create index
+		IIndex index = CCorePlugin.getIndexManager().getIndex(tu.getCProject());
+		// get AST
+		IASTTranslationUnit ast = tu.getAST(index, ITranslationUnit.AST_SKIP_INDEXED_HEADERS);
+		ast.accept(this);
+		return ast;
+	}
+	
+	
+	///////////////////////////////////////////////
+	//// Tree analyser
+	///////////////////////////////////////////////
+//    private static IASTNode currentNode;
+    private final static Logger LOG = Logger.getLogger("Analyser"); 
+
+	/**
+	 * Find the first node that is instance of <b>targetClass</b> and has the name <b>targetName</b>
+     * @param node
+     * @param index
+     * @param targetClass
+     * @param targetName
+     * @return
+     */
+	protected  IASTNode findNodeInTree(IASTNode node, int index, Class<?> targetClass, String targetName) {
 		try {
-			for (IPathEntry child : project.getResolvedPathEntries()){
-				System.out.println(child.getPath().toString());
+			if (targetClass== null || targetName==null)
+				throw new IllegalArgumentException("Target class or target name is NULL!");
+	
+			//get node's children
+			IASTNode[] children = node.getChildren();
+		
+			//navigate recursively
+			for (IASTNode childNode : children){
+				IASTNode tempNode = findNodeInTree(childNode, index+1, targetClass, targetName);
+				if (tempNode != null)
+					return tempNode;
 			}
-		} catch (CModelException e) {
+	
+//			if (children.length==0)
+//				System.out.println("Node:\t" + currentNode.getRawSignature());
+	
+			//find node of class targetClass with name targetName
+			if (targetClass.isInstance(node)){
+				Method method = targetClass.getMethod("getName", (Class<?>[]) null);
+				if (targetName.equals(method.invoke(node).toString())){
+					System.out.println("FOUND");
+					return node;
+				}
+			}
+		}//try
+		catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			e.printStackTrace();
 		}
-		
-//		if (parser == null){
-//			FileContent fileContent 					= FileContent.createForExternalFileLocation(file.getRawLocation().toString());
-//			IScannerInfo info							= new ScannerInfo(new HashMap<String, String>());
-//			IncludeFileContentProvider emptyIncludes	= IncludeFileContentProvider.getEmptyFilesProvider();
-//			IIndex index								= EmptyCIndex.INSTANCE;
-//			int options									= ITranslationUnit.AST_SKIP_ALL_HEADERS | GPPLanguage.OPTION_NO_IMAGE_LOCATIONS | GPPLanguage.OPTION_IS_SOURCE_UNIT;
-//	        IParserLogService log 						= new DefaultLogService();
-//
-//			parser = GPPLanguage.getDefault().getASTTranslationUnit(fileContent, info, emptyIncludes, index, options , log);
-//		}
 		return null;
 	}
+	
+	
+	
+	/**
+	 * Find all nodes that are instances of <b>targetClass</b>
+     * @param node
+     * @param index
+     * @param targetClass
+     * @param targetName
+     * @return
+     */
+	protected  void findAllNodesInTree(IASTNode node, int index, Class<?> targetClass, List<? super IASTNode> nodes) {
+		try {
+			if (targetClass== null)
+				throw new IllegalArgumentException("Target class is NULL!");
+	
+			//get node's children
+			IASTNode[] children = node.getChildren();
+		
+			//navigate recursively
+			for (IASTNode childNode : children){
+				findAllNodesInTree(childNode, index+1, targetClass, nodes);
+			}
+	
+			//find node of class targetClass with name targetName
+			if (targetClass.isInstance(node)){
+				System.out.println("FOUND: " + node.getRawSignature());
+				nodes.add(node);
+			}
+		}//try
+		catch (SecurityException | IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	
+	///////////////////////////////////////////////
+	//// Epsilon specific functions
+	///////////////////////////////////////////////
+	
+	public Collection<Object> getAllofType(String type){
+		List<Object> nodes = new ArrayList<Object>(); 
+		try {
+			System.out.println(getClass().getSimpleName() +".getAllofType(..)");
+			findAllNodesInTree(mainAST, 1, Class.forName("org.eclipse.cdt.core.dom.ast.IAST" + type), nodes);
+		} 
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return nodes;
+	}
+	
+	
+	@Override
+	protected int genericVisit(IASTNode node) {
+//			System.out.println(node.getRawSignature());
+		return PROCESS_CONTINUE;
+	}
+
+	
+	@Override
+	public int visit(IASTDeclarator declarator) {
+//		if (declarator instanceof IASTFunctionDeclarator){
+//			System.out.println(declarator.getRawSignature());
+//			if (declarator.getName().toString().equals("main")){
+//				if (mainTU == null)
+//					mainTU = declarator.getTranslationUnit().getOriginatingTranslationUnit();
+//				else{					
+//					try {
+//						throw new NoSuchMethodException(String.format("Multiple main() methods in project %s", cproject.getProject().getName()));
+//					} 
+//					catch (NoSuchMethodException e) {
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//		}
+		return PROCESS_CONTINUE;
+	}
+
 }
