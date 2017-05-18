8cf2be4492abfec7c2748437453809a79f0c5667
Simos Gerasimou <simos@cs.york.ac.uk>
2017-05-18 18:48:09 +0100

    - improvements in ReflectiveASTVisitor

--- org.eclipse.epsilon.emc.cdt/src/org/eclipse/epsilon/emc/cdt/ReflectiveASTVisitor.java
+++ org.eclipse.epsilon.emc.cdt/src/org/eclipse/epsilon/emc/cdt/ReflectiveASTVisitor.java
@@ -123,7 +123,85 @@ public class ReflectiveASTVisitor extends ASTGenericVisitor {
 	//// Epsilon specific functions
 	///////////////////////////////////////////////
 	
+	protected Collection<Object> getAllofKind(String kind){
+		checkOfKind = true;
+		return getAllVisitor(kind);
+	}
+	
+	
 	protected Collection<Object> getAllofType(String type){
+		checkOfKind = false;
+		return getAllVisitor(type);
+	}
+	
+	
+	private  Collection<Object> getAllVisitor(String type){
+		List<Object> nodes = new ArrayList<Object>();
+		try {
+			for (ITranslationUnit tu : tuList){
+				//check if tu is null
+				if (tu == null){
+					Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+					MessageDialog.openInformation( shell, "Unexpected error in translation unit", "Unexpected error in translation unit (NULL)");
+					throw new NullPointerException("Unexpected error in translation unit (NULL)");
+				}
+				//if its AST is not in the cache
+				IASTTranslationUnit ast = projectASTCache.get(tu); 
+				if (projectASTCache.get(tu)==null){
+					// generate the AST and put it in cache
+						ast = tu.getAST(projectIndex, ITranslationUnit.AST_SKIP_INDEXED_HEADERS);
+					projectASTCache.put(tu, ast);
+				}
+				
+				ast.accept(new ASTGenericVisitor(true) {
+					@Override
+					protected int genericVisit(IASTNode node) {
+						if (node.getClass().getSimpleName().equals(type)){							
+							nodes.add(node);
+						}
+						else if (checkNode(node.getClass().getInterfaces(), type)){
+							nodes.add(node);
+						}
+						else if (checkOfKind){
+							Class<?> superClass = node.getClass().getSuperclass();
+							while (superClass != Object.class) {
+								if (superClass.getSimpleName().equals(type)) {
+									nodes.add(node);
+									break;
+								}
+								else if (checkNode(superClass.getInterfaces(), type)){
+									nodes.add(node);
+									break;
+								}
+								superClass = superClass.getSuperclass();
+							}
+						}
+						return PROCESS_CONTINUE;
+					}		
+				});
+			}
+		} 
+		catch (CoreException e) {
+			e.printStackTrace();
+		}
+		return nodes;		
+	}
+	
+	private boolean checkNode(Class<?>[] interfacesArrays, String type ){
+		Queue<Class<?>> interfaces = new LinkedList<Class<?>>(Arrays.asList(interfacesArrays));
+		while (!interfaces.isEmpty()){
+			Class<?> inteface  = interfaces.remove(); 
+//			System.out.println(inteface.getSimpleName());
+			if (inteface.getSimpleName().equals(type)){
+				return true;
+			}
+			interfaces.addAll(Arrays.asList(inteface.getInterfaces()));
+		}
+		return false;
+	}
+	
+	
+	protected Collection<Object> getAllofTypeRecursion(String type){
 		List<Object> nodes = new ArrayList<Object>(); 
 		try {
 //			System.out.println(getClass().getSimpleName() +".getAllofType(..)");
@@ -144,6 +222,7 @@ public class ReflectiveASTVisitor extends ASTGenericVisitor {
 				}
 			}
 		}
+		
 		return nodes;
 	}
 	
@@ -164,49 +243,168 @@ public class ReflectiveASTVisitor extends ASTGenericVisitor {
 	}
 	
 	
-	protected Collection<Object> getAllofTypeVisitor(String type){
-		List<Object> nodes = new ArrayList<Object>();
-		mainAST.accept(new ASTVisitor() {
-			{
-				shouldVisitTranslationUnit = true;
-				shouldVisitNamespaces   = true;
-			}
-			
-			@Override
-			public int visit(ICPPASTNamespaceDefinition namespaceDefinition) {
-				nodes.add(namespaceDefinition);
-				return PROCESS_CONTINUE;
-			}
-			@Override
-			public int visit(IASTTranslationUnit iast) {
-				System.out.println(iast.getOriginatingTranslationUnit());
-				return PROCESS_CONTINUE;
-			}
-		});
-		return nodes;
-	}
+	
+	////////////////////////////////////////////////////////
+	//
+	//Reduntant functions
+	//
+	////////////////////////////////////////////////////////
+//	/**
+//	 * Find the source file with main() function
+//	 * @param cProject
+//	 * @return
+//	 * @throws NoSuchMethodException
+//	 * @throws UnexpectedException
+//	 * @throws CoreException
+//	 * @throws ClassNotFoundException 
+//	 */
+//	@Deprecated
+//	private void findMainFunction() throws NoSuchMethodException, UnexpectedException, CoreException, ClassNotFoundException{
+//		//get source folders
+//		for (ISourceRoot sourceRoot : cproject.getSourceRoots()){
+//			System.out.println(sourceRoot.getLocationURI().getPath());
+//			//get all elements
+//			for (ICElement element : sourceRoot.getChildren()){
+////				System.out.println(element.getElementName() +"\t"+ element.getElementType() +"\t"+element.getClass());
+//				//find a .c or cpp file
+//				if (element instanceof ITranslationUnit && ((ITranslationUnit) element).isSourceUnit()){
+//					ITranslationUnit tu		= (ITranslationUnit) element;
+//					IASTTranslationUnit ast = getASTFromtTranslationUnit(tu);
+//					IASTNode node = ASTUtilities.findNodeInTree(ast, 1, Class.forName("org.eclipse.cdt.core.dom.ast.IASTFunctionDeclarator"), "getName", "main");
+//					if (node != null && mainTU == null){
+//						mainTU 		= tu;
+//						mainAST 	= ast;
+//					}
+//					else if (node != null && mainTU != null){
+//						throw new NoSuchMethodException(String.format("Multiple main() methods in project %s", cproject.getProject().getName()));
+//					} 
+//				}
+//				
+//			}
+//		}
+//		
+//		if (mainTU ==null)
+//			throw new NoSuchMethodException(String.format("main() method not found in project %s", cproject.getProject().getName()));
+//		return;
+//	}
+
+
+//	/**
+//	 * Return the AST for a project
+//	 * 
+//	 * @return IASTTranslationUnit
+//	 * @throws CoreException 
+//	 * @throws UnexpectedException 
+//	 */
+//	@Deprecated
+//	protected IASTTranslationUnit getASTForFile(IFile file) throws CoreException, UnexpectedException {		
+//		// 1) get AST for a workspace file
+//		// if (parser == null){
+//		// //Create translation unit for file
+//		// ITranslationUnit tu = CoreModelUtil.findTranslationUnit(file);
+//		// if (tu == null){
+//		// Shell shell
+//		// =PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+//		// MessageDialog.openInformation( shell, "Unexpected error in
+//		// translation unit", "Unexpected error in translation unit (NULL)");
+//		// throw new UnexpectedException("Unexpected error in translation unit
+//		// (NULL)");
+//		// }
+//		// //get AST
+//		// IASTTranslationUnit iast = tu.getAST();
+//		// }
+//
+//		// 2) get an AST for a code reader (i.e., adding additional properties)
+//		// if (parser == null){
+//		// FileContent fileContent =
+//		// FileContent.createForExternalFileLocation(file.getRawLocation().toString());
+//		// IScannerInfo info = new ScannerInfo(new HashMap<String, String>());
+//		// IncludeFileContentProvider emptyIncludes =
+//		// IncludeFileContentProvider.getEmptyFilesProvider();
+//		// IIndex index = EmptyCIndex.INSTANCE;
+//		// int options = ITranslationUnit.AST_SKIP_ALL_HEADERS |
+//		// GPPLanguage.OPTION_NO_IMAGE_LOCATIONS |
+//		// GPPLanguage.OPTION_IS_SOURCE_UNIT;
+//		// IParserLogService log = new DefaultLogService();
+//		//
+//		// parser = GPPLanguage.getDefault().getASTTranslationUnit(fileContent,
+//		// info, emptyIncludes, index, options , log);
+//		// }
+//
+//		// 3) get an index-based AST for a code reader
+//		if (mainTU == null) {
+//			// Create translation unit for file
+//			ITranslationUnit tu = CoreModelUtil.findTranslationUnit(file);
+//			if (tu == null) {
+//				Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+//				MessageDialog.openInformation(shell, "Unexpected error in translation unit",
+//						"Unexpected error in translation unit (NULL)");
+//				throw new UnexpectedException("Unexpected error in translation unit (NULL)");
+//			}
+//			//create index
+//			IIndex index = CCorePlugin.getIndexManager().getIndex(tu.getCProject());
+//			// get AST
+//			IASTTranslationUnit iast = tu.getAST(index, ITranslationUnit.AST_SKIP_INDEXED_HEADERS);
+//			iast.accept(this);
+//		}
+//		return null;
+//	}
+
+
+//	/**
+//	 * Return AST
+//	 * @return
+//	 */
+//	@Deprecated
+//	protected IASTTranslationUnit getAST(){
+//		return this.mainAST;
+//	}
 	
 	
-	@Override
-	protected int genericVisit(IASTNode node) {
-//			System.out.println(node.getRawSignature());
-		return PROCESS_CONTINUE;
-	}
-	
-	@Override
-	public int visit(IASTDeclarator declarator) {
-		if (declarator instanceof IASTFunctionDeclarator){
-			
-		}
-		return PROCESS_CONTINUE;
-	}
-	
-	@Override
-	public int visit(IASTExpression exp) {
-		if (! (exp instanceof IASTFunctionCallExpression))
-			return PROCESS_CONTINUE;
-		IASTFunctionCallExpression funcCallExp 	= (IASTFunctionCallExpression)exp;
-//		System.out.println(funcCallExp.getRawSignature());
-		return PROCESS_CONTINUE;
-	}
+//	protected Collection<Object> getAllofTypeVisitor(String type){
+//	List<Object> nodes = new ArrayList<Object>();
+//	mainAST.accept(new ASTVisitor() {
+//		{
+//			shouldVisitTranslationUnit = true;
+//			shouldVisitNamespaces   = true;
+//		}
+//		
+//		@Override
+//		public int visit(ICPPASTNamespaceDefinition namespaceDefinition) {
+//			nodes.add(namespaceDefinition);
+//			return PROCESS_CONTINUE;
+//		}
+//		@Override
+//		public int visit(IASTTranslationUnit iast) {
+//			System.out.println(iast.getOriginatingTranslationUnit());
+//			return PROCESS_CONTINUE;
+//		}
+//	});
+//	return nodes;
+//}
+//
+//
+//@Override
+//protected int genericVisit(IASTNode node) {
+////		System.out.println(node.getRawSignature());
+//	return PROCESS_CONTINUE;
+//}
+//
+//@Override
+//public int visit(IASTDeclarator declarator) {
+//	if (declarator instanceof IASTFunctionDeclarator){
+//		
+//	}
+//	return PROCESS_CONTINUE;
+//}
+//
+//@Override
+//public int visit(IASTExpression exp) {
+//	if (! (exp instanceof IASTFunctionCallExpression))
+//		return PROCESS_CONTINUE;
+//	IASTFunctionCallExpression funcCallExp 	= (IASTFunctionCallExpression)exp;
+////	System.out.println(funcCallExp.getRawSignature());
+//	return PROCESS_CONTINUE;
+//}
+
 }
